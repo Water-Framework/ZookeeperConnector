f208603a4b23f4b0d78e07d7b7f348b9c723cbd6
Aristide <aristide.cittadino@acsoftware.it>
2025-04-19 15:53:27 +0200

    adding files for zk connector

---
+++ ZookeeperConnector-cluster-coordinator-client/src/main/java/it/water/connectors/zookeeper/cluster/ZKClusterCoordinatorClient.java
@@ -0,0 +1,48 @@
+package it.water.connectors.zookeeper.cluster;
+
+import it.water.connectors.zookeeper.api.ZookeeperConnectorSystemApi;
+import it.water.core.api.service.cluster.ClusterCoordinatorClient;
+import it.water.core.api.service.cluster.ClusterNodeInfo;
+import it.water.core.api.service.cluster.ClusterNodeOptions;
+import it.water.core.interceptors.annotations.FrameworkComponent;
+import it.water.core.interceptors.annotations.Inject;
+import lombok.Setter;
+
+import java.util.Collection;
+import java.util.List;
+
+@FrameworkComponent
+public class ZKClusterCoordinatorClient implements ClusterCoordinatorClient {
+    @Inject
+    @Setter
+    private ClusterNodeOptions clusterNodeOptions;
+
+    @Inject
+    @Setter
+    private ZookeeperConnectorSystemApi zookeeperConnectorSystemApi;
+
+    @Override
+    public boolean registerToCluster() {
+        return false;
+    }
+
+    @Override
+    public boolean unregisterToCluster() {
+        return false;
+    }
+
+    @Override
+    public boolean checkClusterLeadershipFor(String s) {
+        return false;
+    }
+
+    @Override
+    public boolean peerStillExists(ClusterNodeOptions clusterNodeOptions) {
+        return false;
+    }
+
+    @Override
+    public Collection<ClusterNodeInfo> getPeerNodes(ClusterNodeOptions clusterNodeOptions) {
+        return List.of();
+    }
+}
